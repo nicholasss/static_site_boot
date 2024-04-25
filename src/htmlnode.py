298073d63e8a4b668e1c7fd9811e83d5b655061e
nicholas <nicholaszingleman@gmail.com>
2024-04-25 19:41:28 -0400

    put in place html stubs

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+# HTMLNode class
+
+class HTMLNode:
+
+	def __init__(self, tag=None, value=None, children=None, props=None) -> None:
+		self.tag = tag
+		self.value = value
+		self.children = children
+		self.props = props
+
+	def to_html(self):
+		raise NotImplementedError
+	
+	def props_to_html(self):
+		pass
+
+	def __repr__(self) -> str:
+		pass
---
+++ src/test_htmlnode.py
@@ -0,0 +1,8 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+	def test_props(self):
+		raise NotImplementedError
