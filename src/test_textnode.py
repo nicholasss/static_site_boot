02d51312035331d7875b0cb1b698e479e24243dd
nicholas <nicholaszingleman@gmail.com>
2024-04-25 19:34:39 -0400

    added additional test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,11 +4,22 @@ from textnode import TextNode
 
 
 class TestTextNode(unittest.TestCase):
-    def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
-        self.assertEqual(node, node2)
+	def test_eq(self):
+		node1 = TextNode("This is a text node", "bold")
+		node2 = TextNode("This is a text node", "bold")
+		self.assertEqual(node1, node2, "FAIL")
+        
+		node1 = TextNode("Looping Garbage", "italic", "https://www.google.com")
+		node2 = TextNode("Looping Garbage", "italic", "https://www.google.com")
+		self.assertEqual(node1, node2, "FAIL")
 
+		node1 = TextNode("Granite Heights", "bold")
+		node2 = TextNode("Granite Heights", "italics")
+		self.assertNotEqual(node1, node2, "FAIL")
+
+		node1 = TextNode("Looping Garbage", "italic", "https://www.google.com")
+		node2 = TextNode("Looping Garbage", "italic", "https://www.apple.com")
+		self.assertNotEqual(node1, node2, "FAIL")
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
