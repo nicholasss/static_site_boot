9eb6c4c31ca6eae69a689ffbd39666a2973e769d
nicholas <nicholaszingleman@gmail.com>
2024-04-25 19:49:30 -0400

    working on htmlnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,8 +11,22 @@ class HTMLNode:
 	def to_html(self):
 		raise NotImplementedError
 	
-	def props_to_html(self):
-		pass
+	def props_to_html(self) -> str:
+		#TODO: need to test this
+  
+		html = ""
+		for prop, value in self.props:
+			html += f"{prop}=\"{value}\""
+
+			if self.props[1] != None:
+				html += " "
+		
+		return html
 
 	def __repr__(self) -> str:
-		pass
+		#TODO: need to test this
+
+		tag = self.tag
+		value = self.value
+
+		return f"<{tag}>{value}</{tag}>"
