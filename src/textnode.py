b8b75a05eea82027842c6d4903e66a6135cabd00
nicholas <nicholaszingleman@gmail.com>
2024-04-25 06:25:55 -0400

    init for textnode class

--- src/textnode.py
+++ src/textnode.py
@@ -5,3 +5,12 @@
     # self.text - The text content of the node
     # self.text_type - The type of text this node contains, which is just a string like "bold" or "italic"
     # self.url - The URL of the link or image, if the text is a link. Default to None if nothing is passed in.
+
+class TextNode:
+	
+	def __init__(self, text, text_type, url) -> None:
+		self.text = text
+		self.text_type = text_type
+		self.url = url
+
+		
