200bf2c430aa717879e4c821de02fa9ab5ba8326
nicholasss <nicholaszingleman@gmail.com>
2024-05-02 13:24:26 -0400

    working on new split_nodes_delimiter method

--- src/main.py
+++ src/main.py
@@ -3,26 +3,43 @@ from htmlnode import HTMLNode
 from leafnode import LeafNode
 
 
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 def main():
 	print("Hello, World!")
 
 
 def text_node_to_html_node(text_node):
-	if text_node.text_type == "text":
+	if text_node.text_type == text_type_text:
 		return LeafNode(value=text_node.text)
-	elif text_node.text_type == "bold":
+	elif text_node.text_type == text_type_bold:
 		return LeafNode("b", text_node.text)
-	elif text_node.text_type == "italic":
+	elif text_node.text_type == text_type_italic:
 		return LeafNode("i", text_node.text)
-	elif text_node.text_type == "code":
+	elif text_node.text_type == text_type_code:
 		return LeafNode("code", text_node.text)
-	elif text_node.text_type == "link":
+	elif text_node.text_type == text_type_link:
 		return LeafNode("a", text_node.text, {"href": f"{text_node.url}"})
-	elif text_node.text_type == "image":
+	elif text_node.text_type == text_type_image:
 		return LeafNode("img", props={"src": f"{text_node.url}", "alt": "Default Alt Text"})
 
 	else:
 		raise ValueError("Unable to convert to HTMLNode Class.")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+	new_nodes = []
+
+	for old_node in old_nodes:
+		if type(old_node) is not TextNode:
+			new_nodes.append(old_node)
+
 
 if __name__ == "__main__":
 	main()
