b783da5a0601268fe1a4144363dc723619e2b3fd
nicholas <nicholaszingleman@gmail.com>
2024-05-02 06:01:47 -0400

    added one test case

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -5,20 +5,16 @@ from leafnode import LeafNode
 
 
 class TestParentNode(unittest.TestCase):
-	def test_props(self):
+	def test_to_html(self):
 
 		header = LeafNode("h1", "New Products!")
 		child1 = LeafNode("b", "This is a great deal!")
 		child2 = LeafNode(None, "What an amazing value!")
 
 		para = ParentNode("p", [child1, child2])
-
 		body = ParentNode("body", [header, para])
 
-		# print(para.to_html())
-		# print(body.to_html())
-
-		# self.assertIn("</body>", body)
-		# self.assertIn("</p>", para)
-		# self.assertIn("<b>", body)
+		self.assertTrue("</body>" in body.to_html())
+		self.assertTrue("</p>" in para.to_html())
+		self.assertTrue("<b>" in body.to_html())
 
