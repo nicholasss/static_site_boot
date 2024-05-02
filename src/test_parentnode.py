0f2599c682a1580ed45f9b99648c9cee5b62e116
nicholas <nicholaszingleman@gmail.com>
2024-05-02 06:07:02 -0400

    added additional ParentNode test case

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -18,3 +18,15 @@ class TestParentNode(unittest.TestCase):
 		self.assertTrue("</p>" in para.to_html())
 		self.assertTrue("<b>" in body.to_html())
 
+	def test_class_errors(self):
+
+		# Parent class, no children
+		with self.assertRaises(ValueError):
+			para = ParentNode("p")
+
+		# Parent class, no tag
+		child1 = LeafNode("b", "This is a great deal!")
+		child2 = LeafNode(None, "What an amazing value!")
+
+		with self.assertRaises(ValueError):
+			body = ParentNode(None, [child1, child2])
