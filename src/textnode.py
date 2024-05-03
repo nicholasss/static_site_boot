f7802a33a18da6c9fcd651519d8ce1c36f25486a
nicholasss <nicholaszingleman@gmail.com>
2024-05-03 14:07:03 -0400

    completed refactoring of textnode class file to now contain related functions

--- src/textnode.py
+++ src/textnode.py
@@ -45,3 +45,46 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
 
 	else:
 		raise ValueError("Unable to convert to HTMLNode Class.")
+
+
+# This func supports only a single level of nesting.
+# i.e. Cannot process "Oh, the **weather *outside* is frightful**"
+#
+# Func received markdown formattted strings, and converts to to TextNodes
+#
+# old_nodes must be array type
+#
+def split_nodes_delimiter(old_nodes, delimiter: str, text_type: str):
+	new_nodes = []
+
+	for old_node in old_nodes:
+		processed_words = []
+
+		# TODO Test
+		# If its not a TextNode, then add, as is.
+		if type(old_node) is not TextNode:
+			print(f" --NOT TEXTNODE-- {old_node}")
+			new_nodes.append(old_node)
+
+		# Find the position of words within the string, to use later?
+		words = old_node.text.split()
+		for word in words:
+			print(word)
+			if delimiter in word:
+				processed_words.append(word.lstrip(delimiter).rstrip(delimiter))
+			else:
+				processed_words.append(word)
+
+		# Complete processing
+		processed_string = " ".join(processed_words)
+		new_nodes.append(processed_string)
+
+	if len(new_nodes) == 0:
+		return None
+	
+	final_nodes = []
+	for new_node in new_nodes:
+		print(f" --NEW NODE-- {new_node}")
+		final_nodes.append(TextNode(new_node, text_type))
+	
+	return final_nodes
