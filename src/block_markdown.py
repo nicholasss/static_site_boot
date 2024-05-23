75a41aca5ed087aa78338b8fabfb53d7a964b713
nicholasss <nicholaszingleman@gmail.com>
2024-05-23 13:42:26 -0400

    added markdown_to_blocks

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,1 +1,7 @@
 # Chapter 4, part 1: "split blocks"
+
+# Blocks are seperated by a single blank line
+
+def markdown_to_blocks(text: str) -> list[str]:
+	"""This function takes in a large text and will return each block as a seperate string type in a list"""
+	return text.split("\n\n")
