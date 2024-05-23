10b1dc7b47c66c2abe6e158dcfa4a81ae5bf56a0
nicholasss <nicholaszingleman@gmail.com>
2024-05-23 13:58:34 -0400

    part way through block_to_block_type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -2,6 +2,13 @@
 
 # Blocks are seperated by a single blank line
 
+bt_paragraph = "paragraph"
+bt_heading = "heading"
+bt_code = "code"
+bt_quote = "quote"
+bt_ulist = "unordered_list"
+bt_olist = "orderd_list"
+
 def markdown_to_blocks(text: str) -> list[str]:
 	"""This function takes in a large text and will return each block as a seperate string type in a list"""
 	split_blocks = []
@@ -15,3 +22,19 @@ def markdown_to_blocks(text: str) -> list[str]:
 			complete_blocks.append(block.strip())
 	
 	return complete_blocks
+
+def block_to_block_type(block: str) -> str:
+	"""This function returns the type of block from a given block string"""
+	lines = []
+	if "\n" in block:
+		lines = block.split("\n")
+	else:
+		lines = [block]
+	
+	for line in lines:
+		
+		# Headings must have 1-6 hashes and a trailing space
+		if "# " in line[:7]:
+			return bt_heading
+		
+		# Code Blocks or ```\n```
