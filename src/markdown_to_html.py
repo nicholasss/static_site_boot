272f9f34e0853d7f55a4bc70c0b9df734b59f60a
nicholasss <nicholaszingleman@gmail.com>
2024-08-14 15:31:35 -0400

    forgot to use html nodes!

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -13,9 +13,22 @@ DEBUG_PRINT = True
 # Block Types to HTML Node
 # ================================
 
-# Paragraph
-
 # Heading - '# ' --> '###### '
+def heading_block(block_string: str):
+	heading_count = 0
+	for char in block_string:
+		if char == "#":
+			heading_count =+ 1
+		if char == " ":
+			break
+
+	string_start = heading_count + 1
+	heading_raw = block_string[string_start:]
+
+	if DEBUG_PRINT:
+		print(heading_raw)
+	
+	return f'<h{heading_count}>{heading_raw}</{heading_count}>'
 
 # Code - ' ```code``` `
 
@@ -24,6 +37,9 @@ DEBUG_PRINT = True
 # Unordered List - '- ' or '* ' on multiple lines
 
 # Ordered List - '1. ' on multiple lines (ascending)
+
+# Paragraph
+
 
 # ================================
 # Conversion Function
@@ -36,5 +52,17 @@ def markdown_to_html_node(markdown: str):
 	if DEBUG_PRINT:
 		print(markdown_blocks)
 
-	pass
+	html_concat = []
+	for block in markdown_blocks:
+		block_type = block_to_block_type(block)
+
+		if block_type == bt_heading:
+			html_concat.append(heading_block(block))
+	
+	html_concat_len = len(html_concat)
+	html_concat[0] = "<div>" + html_concat[0]
+	html = '\n'.join(html_concat)
+	html += "</div>"
+
+	return html
 
--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -2,20 +2,25 @@ import unittest
 from markdown_to_html import *
 
 class TestMarkdown_To_Html(unittest.TestCase):
-	def test(self):
-		blocks = """# Heading One
+# 	def test(self):
+# 		blocks = """# Heading One
 
-### Heading Three
+# ### Heading Three
 
-**Look at what is to see!**
+# **Look at what is to see!**
 
-```This is some code```"""
+# ```This is some code```"""
 
-		html = markdown_to_html_node(blocks)
+# 		html = markdown_to_html_node(blocks)
 	
-		goal_html = """<h1>Heading One</h1>
-<h3>Heading Three</h3>
-<p><b>Look at what is to see!</b></p>
-<p>whereas this frog is in a board room. <img src="https://api.google.com/frogsuit" alt="Frog in suit"></p>
-<code>This is some code</code>"""
-		self.assertEqual(html, goal_html)
+# 		goal_html = """<h1>Heading One</h1>
+# <h3>Heading Three</h3>
+# <p><b>Look at what is to see!</b></p>
+# <code>This is some code</code>"""
+# 		self.assertEqual(html, goal_html)
+
+	def test_heading(self):
+		heading_one = "# Heading One"
+		html_one = "<h1>Heading One<h1/>"
+
+		self.assertEqual(markdown_to_blocks())
