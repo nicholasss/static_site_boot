e2a358fe512cc15af8db1551f17d3f7be77891e5
nicholasss <nicholaszingleman@gmail.com>
2024-08-13 21:23:51 -0400

    added test class

--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -1,3 +1,13 @@
 import unittest
 from markdown_to_html import *
 
+class TestMarkdown_To_Html(unittest.TestCase):
+	def test(self):
+		blocks = """This frog is on mars: ![Frog on mars](https://api.google.com/frogmars)
+
+		**Look at what is to see!**
+		
+		whereas this frog is in a board room. ![Frog in suit](https://api.google.com/frogsuit)"""
+
+		html = markdown_to_html_node(blocks)
+	
